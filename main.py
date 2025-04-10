f733864145f68b292138ad16bb574cd926bc28a6
yashkasat96 <52992873+yashkasat96@users.noreply.github.com>
2025-04-10 10:54:21 +0530

    Revert "Adding square function"

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from code.sample_code import add_number, subtract_number, multiply_number, square_number
+from code.sample_code import add_number, subtract_number, multiply_number
 
 print(add_number(2, 3))
 print(multiply_number(1, 2))
 print(subtract_number(5, 2))
-print(square_number(5))
