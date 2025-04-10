5848078ea85c07ea4ee8582960c82513e2cf808d
yashkasatgsg <yash.kasat@gsgcontractor.com>
2025-04-10 10:51:32 +0530

    Adding square function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from code.sample_code import add_number, subtract_number, multiply_number
+from code.sample_code import add_number, subtract_number, multiply_number, square_number
 
 print(add_number(2, 3))
 print(multiply_number(1, 2))
 print(subtract_number(5, 2))
+print(square_number(5))
